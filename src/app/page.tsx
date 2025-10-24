8b8c9e77138ed617481a3f5df22bfc72f29622fb
Park Chan Hyeok <56295771+WithM2@users.noreply.github.com>
2025-10-24 16:27:23 +0900

    Add parent review section to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { ReviewSection } from "@/components/ReviewSection";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="font-sans grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 sm:p-20">
+      <main className="row-start-2 flex w-full flex-col items-center gap-10 sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -50,6 +51,8 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+
+        <ReviewSection />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
